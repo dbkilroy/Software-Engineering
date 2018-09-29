750a97e9f6911f3a5c9c9ad1d0f96c9b0ca76632
kilroyda <kilroyda@tcd.ie>
2018-09-29 13:23:33 +0100

    Sample test files added to practice testing in Rust

--- assignment_1/src/main.rs
+++ assignment_1/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
     println!("Hello, world!");
 }
+
+//Pseudo-Code for proposed solution
+/*
+ *  Node LCA(Node root, Node n1, Node n2) {
+ *      if (root == null) { return null; }
+ *      if (root == n1 || root == n2) { return root; }
+ *
+ *      Node left = LCA(root left, n1, n2);
+ *      Node right = LCA(rott right, n1, n2);
+ *      if (left != null && right != null) { return root; }
+ *      if (left == null && right == null) { return null; }
+ *
+ *      return left != null ? left : right;
+ */
+
+#[cfg(test)]
+ mod tests {
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
+}
