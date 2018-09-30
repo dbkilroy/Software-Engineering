b79bb5f06551c4488d926eb689b18e86c95bb0b2
kilroyda <kilroyda@tcd.ie>
2018-09-30 15:19:05 +0100

    Added dependency to graphing library + empty test

--- assignment_1/src/main.rs
+++ assignment_1/src/main.rs
@@ -1,5 +1,14 @@
+extern crate petgraph;
+use petgraph::Graph;
+use std::ptr;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn lca(mut graph: Graph<&str, &str>, x: i32, y: i32) {
+        let a = graph.add_node("x");
+        let b = graph.add_node("y");
 }
 
 //Pseudo-Code for proposed solution
@@ -22,9 +31,37 @@ fn main() {
     fn exploration() {
         assert_eq!(2 + 2, 4);
     }
+    // #[test]
+    // fn larger_can_hold_smaller() {
+    //     let larger = Rectangle { length: 8, width: 7 };
+    //     let smaller = Rectangle { length: 5, width: 1 };
+    //
+    //     assert!(larger.can_hold(&smaller));
+    // }
 
+    // fn build_graph() {
+    //     let mut deps = Graph::<&str, &str>::new();
+    //     let pg = deps.add_node("petgraph");
+    //     let fb = deps.add_node("fixedbitset");
+    //     let qc = deps.add_node("quickcheck");
+    //     let rand = deps.add_node("rand");
+    //     let libc = deps.add_node("libc");
+    //     deps.extend_with_edges(&[
+    //         (pg, fb), (pg, qc),
+    //         (qc, rand), (rand, libc), (qc, libc),
+    //     ]);
+    // }
     #[test]
-    fn another() {
-        panic!("Make this test fail");
+    fn test_empty() {
+        use super::*;
+        let mut graph = Graph::<&str, &str>::new();
+        let a = 1;
+        let b = 2;
+
+        assert_eq!(lca(graph, a, b), NULL, "Empty graph should return null as LCA");
+
+        assert!((lca(graph, a, b).is_null());
     }
+
+
 }
